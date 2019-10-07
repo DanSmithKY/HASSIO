4dd1debc78f0cbd77708f363fc203e165668e1a1
DanSmithKY <DanSmithKY@outlook.com>
2019-10-07 16:37:50 -0500

    Added new git commit script. Blacklisted some recorder entries. (Re?)fixed introduction.yaml.

---
+++ gitcommit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+hassio homeassistant check
+
+git add .
+git status
+
+echo -n "Enter commit description:"
+read COMMIT_DESC
+
+git commit -m "${COMMIT_DESC}"
+git push origin master
+
+exit
