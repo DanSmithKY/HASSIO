3454cfba5e56e155a3ed92f8e7e79dfce5da588e
DanSmithKY <DanSmithKY@outlook.com>
2019-10-07 16:49:59 -0500

    Updated git commit script.

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+echo -n "Checking Home Assistant configuration: "
 hassio homeassistant check
 
 git add .
 git status
 
-echo -n "Enter commit description:"
+echo -n "Enter commit description: "
 read COMMIT_DESC
 
 git commit -m "${COMMIT_DESC}"
